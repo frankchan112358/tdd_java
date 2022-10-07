d8bcede257b4737dd56bd086d856bef0e161241d
Frank Chan <frank.chan@hy-tech.com.tw>
2022-10-07 23:36:43 +0800

    tdd

--- src/main/java/com/hyt/tennis/Tennis.java
+++ src/main/java/com/hyt/tennis/Tennis.java
@@ -1,17 +1,85 @@
 package com.hyt.tennis;
 
+import java.util.HashMap;
+
 public class Tennis {
+    private HashMap<Integer, String> lookupScore = new HashMap<>() {{
+        put(0, "love");
+        put(1, "fifteen");
+        put(2, "thirty");
+        put(3, "forty");
+    }};
+
+    private int firstPlayerScoreTimes;
+    private int secondPlayerScoreTimes;
+    private String firstPlayerName;
+    private String secondPlayerName;
 
     public Tennis(String firstPlayerName, String secondPlayerName) {
+        this.firstPlayerName = firstPlayerName;
+        this.secondPlayerName = secondPlayerName;
     }
 
     public String score() {
-        return null;
+        if (isDifferentScore()) {
+            if (isReadyForGamePoint()) {
+                return isAdv() ? advScore() : winScore();
+            }
+
+            return lookupScore();
+        }
+
+        return isDeuce() ? deuceScore() : sameScore();
+
+    }
+
+    private boolean isDifferentScore() {
+        return firstPlayerScoreTimes != secondPlayerScoreTimes;
+    }
+
+    private String sameScore() {
+        return String.format("%s all", lookupScore.get(firstPlayerScoreTimes));
+    }
+
+    private String deuceScore() {
+        return "deuce";
+    }
+
+    private boolean isDeuce() {
+        return firstPlayerScoreTimes >= 3;
+    }
+
+    private String lookupScore() {
+        return String.format("%s %s", lookupScore.get(firstPlayerScoreTimes), lookupScore.get(secondPlayerScoreTimes));
+    }
+
+    private String winScore() {
+        return advPlayer() + " win";
+    }
+
+    private String advScore() {
+        return advPlayer() + " adv";
+    }
+
+    private boolean isReadyForGamePoint() {
+        return firstPlayerScoreTimes > 3 || secondPlayerScoreTimes > 3;
+    }
+
+    private boolean isAdv() {
+        return Math.abs(firstPlayerScoreTimes - secondPlayerScoreTimes) == 1;
+    }
+
+    private String advPlayer() {
+        return firstPlayerScoreTimes > secondPlayerScoreTimes
+                                ? firstPlayerName
+                                : secondPlayerName;
     }
 
     public void firstPlayerScore() {
+        firstPlayerScoreTimes++;
     }
 
     public void secondPlayerScore() {
+        secondPlayerScoreTimes++;
     }
 }
--- src/test/java/com/hyt/tennis/TennisTests.java
+++ src/test/java/com/hyt/tennis/TennisTests.java
@@ -1,4 +1,112 @@
 package com.hyt.tennis;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class TennisTests {
+
+    private Tennis tennis = new Tennis("tom", "jerry");
+
+    @Test
+    void love_all() {
+        scoreShouldBe("love all");
+    }
+
+    @Test
+    void fifteen_love() {
+        givenFirstPlayerScore(1);
+        scoreShouldBe("fifteen love");
+    }
+
+    @Test
+    void thirty_love() {
+        givenFirstPlayerScore(2);
+        scoreShouldBe("thirty love");
+    }
+
+    @Test
+    void forty_love() {
+        givenFirstPlayerScore(3);
+        scoreShouldBe("forty love");
+    }
+
+    @Test
+    void love_fifteen() {
+        givenSecondPlayerScore(1);
+        scoreShouldBe("love fifteen");
+    }
+
+    @Test
+    void love_thirty() {
+        givenSecondPlayerScore(2);
+        scoreShouldBe("love thirty");
+    }
+
+    @Test
+    void fifteen_all() {
+        givenFirstPlayerScore(1);
+        givenSecondPlayerScore(1);
+        scoreShouldBe("fifteen all");
+    }
+
+    @Test
+    void thirty_all() {
+        givenFirstPlayerScore(2);
+        givenSecondPlayerScore(2);
+        scoreShouldBe("thirty all");
+    }
+
+    @Test
+    void deuce() {
+        givenDeuce();
+        scoreShouldBe("deuce");
+    }
+
+    @Test
+    void firstPlayer_adv() {
+        givenDeuce();
+        givenFirstPlayerScore(1);
+        scoreShouldBe("tom adv");
+    }
+
+    @Test
+    void secondPlayer_adv() {
+        givenDeuce();
+        givenSecondPlayerScore(1);
+        scoreShouldBe("jerry adv");
+    }
+
+    @Test
+    void secondPlayer_win() {
+        givenDeuce();
+        givenSecondPlayerScore(2);
+        scoreShouldBe("jerry win");
+    }
+
+    @Test
+    void firstPlayer_win_without_deuce() {
+        givenFirstPlayerScore(4);
+        scoreShouldBe("tom win");
+    }
+
+    private void givenDeuce() {
+        givenFirstPlayerScore(3);
+        givenSecondPlayerScore(3);
+    }
+
+    private void givenSecondPlayerScore(int times) {
+        for (int i = 0; i < times; i++) {
+            tennis.secondPlayerScore();
+        }
+    }
+
+    private void givenFirstPlayerScore(int times) {
+        for (int i = 0; i < times; i++) {
+            tennis.firstPlayerScore();
+        }
+    }
+
+    private void scoreShouldBe(String expected) {
+        Assertions.assertEquals(expected, tennis.score());
+    }
 }
